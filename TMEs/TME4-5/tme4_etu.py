192d1f3d6afbce24a67978ddb10b2d27bd5edc92
Ahmed Tidiane BALDE <baldeahmedtidiane36@gmail.com>
2018-04-09 08:28:20 +0200

    nothing new

--- TMEs/TME4-5/tme4_etu.py
+++ TMEs/TME4-5/tme4_etu.py
@@ -6,7 +6,6 @@ import matplotlib.animation as animation
 import time
 
 
-
 def decorator_vec(fonc):
     def vecfonc(datax,datay,w,*args,**kwargs):
         if not hasattr(datay,"__len__"):
@@ -18,7 +17,7 @@ def decorator_vec(fonc):
 
 def allow_bias(datax):
     """ Ajoute une colonne de remplie de 1 à datax et retourne la nouvelle
-        matrice correspondante.    
+        matrice correspondante.
     """
     adjust = np.ones(datax.shape[0]).reshape(-1, 1)
     return np.hstack((datax, adjust))
@@ -27,7 +26,7 @@ def allow_bias(datax):
 @decorator_vec
 def mse(datax, datay, w):
     """ retourne la moyenne de l'erreur aux moindres carres """
-    # Ajouter une colonne de remplie de 1 dans datax    
+    # Ajouter une colonne de remplie de 1 dans datax
     if w.shape[1] > datax.shape[1]: datax = allow_bias(datax)
     # (w.x - y)² np.mean(((datax * w) - datay)**2)
     return np.mean((np.dot(datax, w.T) - datay)**2)
@@ -36,10 +35,9 @@ def mse(datax, datay, w):
 @decorator_vec
 def mse_g(datax, datay, w):
     """ retourne le gradient moyen de l'erreur au moindres carres """
-    if w.shape[1] > datax.shape[1]: datax = allow_bias(datax) 
+    if w.shape[1] > datax.shape[1]: datax = allow_bias(datax)
     # 2 x (w.x - y)²
     return np.mean(2 * ((np.dot(datax, w.T)) - datay)**2)
-
 
 @decorator_vec
 def hinge(datax, datay, w):
@@ -53,7 +51,7 @@ def hinge(datax, datay, w):
 @decorator_vec
 def hinge_g(datax, datay, w):
     """ retourne le gradient moyen de l'erreur hinge """
-    
+
     if w.shape[1] > datax.shape[1]: datax = allow_bias(datax)
     h = np.array((datay * np.dot(datax, w.T)) < 0)
     return np.mean(h * datax * datay)
@@ -65,7 +63,7 @@ def stochastic(vectorx, vectory, w):
         à chaque itération est appelée descente de gradient stochastique ou
         Stochastic gradient descent.
     """
-    
+
     return ((np.dot(vectorx.reshape(1,-1), w.T) - vectory)**2)/2
 
 
@@ -73,29 +71,29 @@ def stochastic_g(vectorx, vectory, w):
     """ Retourne le gradient de l'erreur aux moindres carres pour UN exemple de
         datax.
     """
-    
-    return (np.dot(vectorx.reshape(1,-1), w.T) - vectory)
-        
 
-    
+    return (np.dot(vectorx.reshape(1,-1), w.T) - vectory)
+
+
+
 class Lineaire(object):
     def __init__(self,loss=mse,loss_g=mse_g,max_iter=1000,eps=0.01, bias=False):
         """ :loss: fonction de cout
             :loss_g: gradient de la fonction de cout
             :max_iter: nombre d'iterations
-            :eps: pas de gradient            
+            :eps: pas de gradient
         """
         self.max_iter, self.eps = max_iter,eps
         self.loss, self.loss_g = loss, loss_g
         self.bias = bias
-    
-    
+
+
     def bias_decorator(fonc):
         def adjust_datax(self, datax, *args, **kwargs):
-            if self.bias: 
+            if self.bias:
                 datax = allow_bias(datax)
             return fonc(self, datax, *args, **kwargs)
-        
+
         return adjust_datax
 
 
@@ -105,7 +103,7 @@ class Lineaire(object):
         self.loss_g = hinge_g
         for i in range(self.max_iter):
             self.w = self.w - (self.eps * self.loss_g(datax, datay, self.w))
-            
+
     def stochastic_fit(self, datax, datay):
         """ Stochastic gradient descent Learning """
         print("===============Stochastic==============\n")
@@ -114,16 +112,16 @@ class Lineaire(object):
         # It's a good thing to shuffle the datax.
         np.random.shuffle(data)
         datax, datay = data[:,:2], data[:,-1]
-        
+
 
         #plt.figure()
         grid,x,y=make_grid(datax,200)
         step = 0
-        for _ in range(self.max_iter):            
+        for _ in range(self.max_iter):
             for vectorx, vectory in zip(datax, datay):
                 self.w -= (self.eps * self.loss_g(vectorx, vectory, self.w))
-                
-                
+
+
     def stochastic_fit_animation(self, datax, datay):
         """ Stochastic gradient descent Learning """
         print("===============Stochastic==============\n")
@@ -131,26 +129,26 @@ class Lineaire(object):
         data = np.hstack((datax, datay))
         # It's a good thing to shuffle the datax.
         np.random.shuffle(data)
-        datax, datay = data[:,:2], data[:,-1]        
+        datax, datay = data[:,:2], data[:,-1]
 
         #plt.figure()
         grid,x,y=make_grid(datax,200)
         step = 0
-        for _ in range(self.max_iter):            
+        for _ in range(self.max_iter):
             for vectorx, vectory in zip(datax, datay):
                 self.w -= (self.eps * self.loss_g(vectorx, vectory, self.w))
-                
-                # Show video of learning                 
+
+                # Show video of learning
                 plt.title("Stochastic animation, step %d"%(step))
                 plt.contourf(x,y,self.predict(grid).reshape(x.shape),colors=('gray','blue'),levels=[-1,0,1])
                 plot_data(datax, datay)
                 plt.pause(0.0000000000000001)
                 step += 1
                 #plt.close()
-                
+
                 #if t == 0: line, = plt.plot(range(len(self.w[0])), self.w[0], 'b-')
                 #else: line.set_ydata(self.w[0])
-    
+
 
     def minibatch_fit(self, datax, datay, batch_size=10):
         """ Mini-Batch gradient descent Learning """
@@ -161,23 +159,23 @@ class Lineaire(object):
                 batchx, batchy = datax[i:i+batch_size], datay[i:i+batch_size]
                 # Et on essaye de progresser avec cela.
                 self.w -= (self.eps * self.loss_g(batchx, batchy, self.w))
-            
-        
+
+
     @bias_decorator
-    def fit(self,datax,datay,testx=None,testy=None,gradient_descent="batch", 
+    def fit(self,datax,datay,testx=None,testy=None,gradient_descent="batch",
             batch_size=10):
         """ :datax: donnees de train
             :datay: label de train
             :testx: donnees de test
             :testy: label de test
-            :method: can be either, batch_fit, stochastic_fit or mini_batch_fit. 
+            :method: can be either, batch_fit, stochastic_fit or mini_batch_fit.
         """
-        
-        N = len(datay) 
+
+        N = len(datay)
         datay = datay.reshape(-1,1)
         D = datax.shape[1]
         self.w = np.random.random((1,D))
-        
+
         # Let's learn !
         if gradient_descent == "batch": self.batch_fit(datax, datay)
         elif gradient_descent == "stochastic": self.stochastic_fit(datax, datay)
@@ -190,10 +188,10 @@ class Lineaire(object):
     def predict(self,datax):
         if len(datax.shape)==1:
             datax = datax.reshape(1,-1)
-            
+
         return np.sign(np.dot(datax, self.w.T))
-                
-        
+
+
     def score(self,datax,datay):
         return (1 - np.mean((self.predict(datax) == datay[:, np.newaxis])))
 
@@ -203,8 +201,8 @@ def load_usps(fn):
         f.readline()
         data = [[float(x) for x in l.split()] for l in f if len(l.split())>2]
     tmp=np.array(data)
-    
-    # Retourne la seconde colonne jusqu'à la dernière tmp[:,1:], ainsi que 
+
+    # Retourne la seconde colonne jusqu'à la dernière tmp[:,1:], ainsi que
     # la première colonne tmp[:,0]
     return tmp[:,1:],tmp[:,0].astype(int)
 
@@ -221,26 +219,26 @@ def plot_error(datax,datay,f,step=10):
 
 
 def extract_usps(fichier, firstclass, secondclass):
-    """ Permet de construire un ensemble de données datax, datay avec les 
+    """ Permet de construire un ensemble de données datax, datay avec les
         données USPS, juste en lui spécifiant en paramètre:
         :param fichier: le fichier,
         :param firstclass: le premier chiffre dont on veut extraire les données
         :param secondclass: le second chiffre.
         :return: datax, datay
     """
-    
+
     pixels, numbers = load_usps(fichier)
     indexes69 = np.hstack(np.where(numbers == i)[0] for i in (firstclass, secondclass))
     pixels69 = pixels[indexes69]
     numbers69 = numbers[indexes69]
     pixels69y = np.ones(pixels69.shape[0])
-    
+
     for i in range(numbers69.shape[0]):
-        if numbers69[i] == firstclass: 
+        if numbers69[i] == firstclass:
             pixels69y[i] = -1
         else:
             pixels69y[i] = 1
-            
+
     return pixels69, pixels69y
 
 
@@ -258,12 +256,12 @@ def learn_plot_perceptron(perceptron, trainx, trainy, testx, testy, gradient_des
 
 def main():
     """ Tracer des isocourbes de l'erreur """
-     
+
     plt.ion()
     trainx,trainy =  gen_arti(nbex=1000,data_type=0,epsilon=0.3)
     testx,testy =  gen_arti(nbex=1000,data_type=0,epsilon=0.3)
     grid,x1list,x2list=make_grid(xmin=-4,xmax=4,ymin=-4,ymax=4)
-    
+
     # Plot error for test
     plt.figure()
     plot_error(trainx,trainy,mse)
@@ -278,33 +276,33 @@ def main():
     learn_plot_perceptron(perceptron, trainx, trainy, testx, testy,
                           gradient_descent = "batch",
                           title = "Batch gradient descent")
-    
+
     # Stochastic gradient descent
     perceptron2 = Lineaire(loss=stochastic,loss_g=stochastic_g,max_iter=1,
                            eps=0.1,bias=False)
     learn_plot_perceptron(perceptron2, trainx, trainy, testx, testy,
                           gradient_descent = "stochastic",
                           title = "Stochastic gradient descent")
-    
+
     # Mini-Batch gradient descent
     perceptron3 = Lineaire(loss=mse,loss_g=mse_g,max_iter=1000,
                            eps=0.1,bias=False)
     learn_plot_perceptron(perceptron3, trainx, trainy, testx, testy,
                           gradient_descent = "minibatch",
                           title = "Mini-Batch gradient descent")
-    
+
     # Stochastic gradient descent Animation
     perceptron2 = Lineaire(loss=stochastic,loss_g=stochastic_g,max_iter=1,
                            eps=0.1,bias=False)
     # learn_plot_perceptron(perceptron2, trainx, trainy, testx, testy,
     #                      gradient_descent = "stochastic_animation",
-    #                      title = "Stochastic gradient descent") 
-    
+    #                      title = "Stochastic gradient descent")
+
     # USPS DATA
 
-    datax, datay = extract_usps("USPS_train.txt", 6, 9) 
+    datax, datay = extract_usps("USPS_train.txt", 6, 9)
     dataTx, dataTy = extract_usps("USPS_test.txt", 6, 9)
-     
+
     # Affichage des données USPS
     show_usps(datax[1])
 
@@ -313,18 +311,16 @@ def main():
     #learn_plot_perceptron(usps_perceptron, datax, datay, dataTx, dataTy,
     #                      gradient_descent = "minibatch",
     #                      title = "Minibatch gradient descent")
-    
+
     #perceptron_usps = Lineaire(hinge,hinge_g,max_iter=1000,eps=0.1)
     #perceptron_usps.fit(datax,datay, gra)
     #print("Erreur : train %f, test %f"% (perceptron_usps.score(datax, datay),perceptron_usps.score(dataTx,dataTy)))
     #plt.figure()
     #plot_frontiere(trainx,perceptron_usps.predict,200)
     #plot_data(datax,datay)
-    
-    
+
+
 
 if __name__=="__main__":
 
     main()
-   
-    
