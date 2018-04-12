3b8765970b32da0912744eca015e8db7b7d45b9d
AhmedTB <baldeahmedtidiane36@gmail.com>
2018-04-13 00:14:01 +0200

    TME6 adding

--- TMEs/TME6/tme6_etu.py
+++ TMEs/TME6/tme6_etu.py
@@ -56,7 +56,7 @@ def comparaison_usps(perceptron):
     plt.suptitle("Matrice de poids")
     weight_matrix(6, 9, fig, perceptron, ax1)
     weight_matrix(1, 8, fig, perceptron, ax2)
-    plt.savefig("weight_matrix_qqlexs_tme6")
+    # plt.savefig("weight_matrix_qqlexs_tme6")
 
     # Matrice de poids 6 vs All
     matrix_one_vs_all(6, perceptron)
@@ -74,7 +74,7 @@ def plot_frontiere_proba(data, f, step=20):
     plt.contourf(x, y, f(grid).reshape(x.shape), 255)
 
 
-def SVM(data_type=0, epsilon=0.3, kernel='linear',
+def SVM(data_type=0, epsilon=0.3, C=10, kernel='linear',
         probability=True, max_iter=100, ax=plt):
     """ Plot ls différents kernels appliqués sur les différents types de données
     """
@@ -82,7 +82,8 @@ def SVM(data_type=0, epsilon=0.3, kernel='linear',
     trainx,trainy =  gen_arti(nbex=1000,data_type=data_type,epsilon=epsilon)
     testx,testy =  gen_arti(nbex=1000,data_type=data_type,epsilon=epsilon)
 
-    s = svm.SVC(kernel=kernel, probability=probability)
+    s = svm.SVC(C=C, kernel=kernel, probability=probability, max_iter=max_iter)
+    
     s.fit(trainx, trainy)
 
     err_train = round(1 - s.score(trainx, trainy), 3)
@@ -129,7 +130,6 @@ def svm_gridSearch(trainx, trainy, testx, testy):
 #-------------------------- Apprentissage multi-classe -------------------------#
 #################################################################################
 
-
 def multiClass(trainx, trainy, testx, testy):
     """ Traitement des cas multiclasses à partir de classifieurs binaires """
 
@@ -144,11 +144,17 @@ def multiClass(trainx, trainy, testx, testy):
     err_UnvsAll_train = round(1 - UnvsAll.score(trainx, trainy), 3)
     err_UnvsAll_test = round(1 - UnvsAll.score(testx, testy), 3)
 
-
+    
     print("Err_1vs1 : train %f, test %f\n"% (err_Unvs1_train, err_Unvs1_test))
     print("Err_1vsAll : train %f, test %f\n"% (err_UnvsAll_train,
                                                err_UnvsAll_test))
 
+    print("========== Prediction UnvsUn ==============")
+    print(Unvs1.predict(testx).shape)
+
+    print("========= Prediction UnvsRest =============")
+    print(UnvsAll.predict(testx).shape)
+    
 
 #################################################################################
 #--------------------------------- String Kernel -------------------------------#
@@ -209,7 +215,7 @@ def main():
     for dtype in range(3):
         linear_model(perceptron, data_type=dtype, epsilon=0.3, ax=ax_lm[dtype])
 
-    plt.savefig("linear_model_perceptron.png")
+    # plt.savefig("linear_model_perceptron.png")
 
     # Application du perceptron de linear model sur les données usps
     comparaison_usps(perceptron)
@@ -219,11 +225,12 @@ def main():
     fig_svm, ax_svm = plt.subplots(ncols=4, nrows=3, sharex=True, figsize=(25,15))
     for dtype in range(3):
         for j, kernel in enumerate(kernels):
-            SVM(data_type=dtype, epsilon=0, kernel=kernel, probability=True,
+            SVM(data_type=dtype, epsilon=0.0001, C=50, kernel=kernel, probability=True,
                 max_iter=100, ax=ax_svm[dtype][j])
 
     fig_svm.tight_layout()
-    plt.savefig("svm_data_kernel.png")
+    # plt.savefig("svm_data_kernel.png")
+    plt.show()
 
 
     #------------------------------- Grid Search
@@ -231,12 +238,12 @@ def main():
     # Données 2D artificiels
     trainx, trainy = gen_arti(nbex=1000, data_type=0, epsilon=0.3)
     testx, testy = gen_arti(nbex=1000, data_type=0, epsilon=0.3)
-    svm_gridSearch(trainx, trainy, testx, testy)
+    # svm_gridSearch(trainx, trainy, testx, testy)
 
     # Données Usps
     trainux, trainuy = load_usps("USPS_train.txt")
     testux, testuy = load_usps("USPS_test.txt")
-    svm_gridSearch(trainux, trainuy, testux, testuy)
+    # svm_gridSearch(trainux, trainuy, testux, testuy)
 
     #------------------------------- Multi Class
 
