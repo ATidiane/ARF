f70044a9a170ecf14ab9ba35a8f65529c4cd38b1
Ahmed Tidiane BALDE <baldeahmedtidiane36@gmail.com>
2018-04-22 11:06:18 +0200

    Starting Inpainting project, visualizing data

---
+++ InpaintingProject/appendix_figures.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import offsetbox
+
+
+def show_usps(data):
+    """ Fonction nous permettant de visualiser les doonées USPS """
+
+    plt.imshow(data.reshape((16, 16)), interpolation="nearest", cmap="YlGnBu")
+    plt.colorbar()
+    plt.show()
+
+
+def plot_components(data, model, images=None, ax=None,
+                    thumb_frac=0.05, cmap='gray'):
+    ax = ax or plt.gca()
+
+    proj = model.fit_transform(data)
+    ax.plot(proj[:, 0], proj[:, 1], '.k')
+
+    if images is not None:
+        min_dist_2 = (thumb_frac * max(proj.max(0) - proj.min(0))) ** 2
+        shown_images = np.array([2 * proj.max(0)])
+        for i in range(data.shape[0]):
+            dist = np.sum((proj[i] - shown_images) ** 2, 1)
+            if np.min(dist) < min_dist_2:
+                # don't show points that are too close
+                continue
+            shown_images = np.vstack([shown_images, proj[i]])
+            imagebox = offsetbox.AnnotationBbox(
+                offsetbox.OffsetImage(images[i], cmap=cmap),
+                proj[i])
+            ax.add_artist(imagebox)
